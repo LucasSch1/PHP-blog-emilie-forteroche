c8daf179c9a5d1c9b1e0fb15d62729eee1f0a1af
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2024-11-29 10:54:50 +0100

    Mise en page en format tableau (a remodifier)

--- views/templates/adminMonitoring.php
+++ views/templates/adminMonitoring.php
@@ -8,14 +8,27 @@
 <h2>Page de monitoring</h2>
 
 <div class="adminArticle">
-    <?php foreach ($articles as $article) { ?>
-        <div class="articleLine">
-            <div class="title"><?= $article->getTitle() ?></div>
-            <div class="view"><?= $article->getViews() ?></div>
-            <div class="commentsCounts"><?= $article->getCommentsCount() ?></div>
-            <div class="publishedDate"><?= $article->getDateCreation()->format('Y-m-d') ?></div>
-        </div>
-    <?php } ?>
+
+    <table style="width: 100%; border-collapse: collapse; text-align: center;">
+        <thead>
+        <tr>
+            <th>Nom de l'article</th>
+            <th>Nombre de vues</th>
+            <th>Nombre de commentaires</th>
+            <th>Date de publication</th>
+        </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($articles as $article) { ?>
+                <tr>
+                    <td><?= htmlspecialchars($article->getTitle()) ?></td>
+                    <td><?= htmlspecialchars($article->getViews()) ?></td>
+                    <td><?= htmlspecialchars($article->getCommentsCount()) ?></td>
+                    <td><?= htmlspecialchars($article->getDateCreation()->format('Y-m-d')) ?></td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
 </div>
 
 <a class="submit" href="index.php?action=showUpdateArticleForm">Ajouter un article</a>
