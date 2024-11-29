5caef371652f4415804b17c98cfe240cb8aaf36d
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2024-11-29 10:55:35 +0100

    Récupération du nombre de commentaires quand on fait appel à tout les articles.

--- models/ArticleManager.php
+++ models/ArticleManager.php
@@ -20,6 +20,28 @@ class ArticleManager extends AbstractEntityManager
         }
         return $articles;
     }
+
+
+    public function getAllArticlesOptions() : array
+    {
+        $sql = "SELECT id, title, date_creation, views  FROM article";
+        $result = $this->db->query($sql);
+        $articles = [];
+
+        while ($articleData = $result->fetch()) {
+            $article = new Article($articleData);
+
+            // Récupère le nombre de commentaires pour cet article
+            $commentsCount = $this->getCommentsCountByArticleId($article->getId());
+
+            // Définit le nombre de commentaires dans l'objet Article
+            $article->setCommentsCount($commentsCount);
+            // Ajoute l'article à la liste
+            $articles[] = $article;
+
+        }
+        return $articles;
+    }
     
     /**
      * Récupère un article par son id.
@@ -107,4 +129,24 @@ class ArticleManager extends AbstractEntityManager
         $stmt->bindParam(':id', $id);
         $stmt->execute();
     }
+
+    public function getCommentsCountByArticleId(int $articleId): int
+    {
+        $sql = "SELECT COUNT(*) as count FROM comment WHERE id_article = :articleId";
+        $stmt = $this->db->query($sql, ['articleId' => $articleId]);
+        // Récupère le résultat
+        $result = $stmt->fetch();
+        return (int)($result['count'] ?? 0);
+    }
+
+
+
+    public function getViewsByArticleId(int $id): int
+    {
+        $sql = "SELECT views FROM article WHERE id = :idArticle";
+        $result = $this->db->query($sql, ['idArticle' => $id]);
+
+        return (int) $result[0]['views'];
+    }
+
 }
