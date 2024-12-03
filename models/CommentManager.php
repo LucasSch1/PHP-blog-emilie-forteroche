70ec19d31c69835078f44142d4918a162b772970
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2024-12-03 13:34:51 +0100

    Ajout de la function qui supprime un commentaire

--- models/CommentManager.php
+++ models/CommentManager.php
@@ -54,16 +54,13 @@ class CommentManager extends AbstractEntityManager
         return $result->rowCount() > 0;
     }
 
-    /**
-     * Supprime un commentaire.
-     * @param Comment $comment : l'objet Comment à supprimer.
-     * @return bool : true si la suppression a réussi, false sinon.
-     */
-    public function deleteComment(Comment $comment) : bool
+
+    public function deleteCommentById(int $commentId): bool
     {
         $sql = "DELETE FROM comment WHERE id = :id";
-        $result = $this->db->query($sql, ['id' => $comment->getId()]);
+        $result = $this->db->query($sql, ['id' => $commentId]);
         return $result->rowCount() > 0;
+
     }
 
 }
